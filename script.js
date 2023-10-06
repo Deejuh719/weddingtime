007e6d88c0fc36c0a3accceae5152f3a6c59ef42
K Surratt <112124288+Deejuh719@users.noreply.github.com>
2023-10-06 17:23:15 -0400

    did i do the thiiiing

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-function typing(element, speed) {
-    var text = element.innerHTML;
-    element.innerHTML = "";
-
-    var i = 0;
-    var timer = setInterval(function() {
-        if(i < text.length) {
-            element.append(text.charAt(i));
-            i++;
-        } else {
-            clearInterval(timer);
-        }
-    }, speed);
-}
-
-var speedH1 = 50;
-var speedP = 50;
-var h1 = document.querySelector('h1');
-var p = document.querySelector('p');
-var delay = h1.innerHTML.length * speedH1 + speedH1;
-
-typing(h1, speed);
-
-setTimeout(function(){
-    p.style.display = "inline-block";
-    typing(p, speedP);
-}, delay);
+document.addEventListener('DOMContentLoaded', function(){
+    function typing(element, speed) {
+        var text = element.innerHTML;
+        element.innerHTML = "";
+    
+        var i = 0;
+        var timer = setInterval(function() {
+            if(i < text.length) {
+                element.append(text.charAt(i));
+                i++;
+            } else {
+                clearInterval(timer);
+            }
+        }, speed);
+    }
+    
+    var speedH1 = 50;
+    var speedP = 50;
+    var h1 = document.querySelector('h1');
+    var p = document.querySelector('p');
+    var delay = h1.innerHTML.length * speedH1 + speedH1;
+    
+    typing(h1, speed);
+    
+    setTimeout(function(){
+        p.style.display = "inline-block";
+        typing(p, speedP);
+    }, delay);
+});
 
 document.onclick=function(){
     document.location.href="invite.html";
