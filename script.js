17567fb07a59a471960a847956837b123743c304
K Surratt <112124288+Deejuh719@users.noreply.github.com>
2023-10-06 16:48:51 -0400

    dfaveavea

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-var text = "Consider This Your Invitation. Click the page, I dare ya!"
+const texts = ['Consider This Your Invitation', 'Click the page, I dare ya!'];
 
-var ele = '<span>' + text.split('').join('</span><span>') + '</span>';
+texts.forEach(text =>
+    {
+        const div = document.createElement('div');
+        let p = document.createElement('p');
+        div.appendChild(p);
+        document.body.appendChild(div);
+    });
+
+function typing(p, index, text) {
+    p.innerHTML = text.substr(0, index);
+    if(index<text.length)
+    {
+        setTimeout(typing, speed, p, index+1, text);
+    }
+}
+/**var ele = '<span>' + text.split('').join('</span><span>') + '</span>';
 
 $(ele).hide().appendTo('div').each(function(i))
 {
@@ -10,7 +25,7 @@ $(ele).hide().appendTo('div').each(function(i))
     }).animate({
         opacity: 1
     },100):
-});
+});*/
 
 document.onclick=function(){
     document.location.href="invite.html";
